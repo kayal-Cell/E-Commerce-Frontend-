f77a712e073f2dbbaddd137c1344edb3422d63f5
Kayal <kayalvizhi3102005@gmail.com>
2026-03-16 21:24:44 +0530

    Updated navbar and search bar

---
+++ E-commerece(frontend)/src/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Navbar.css";  
+function Navbar() {
+  return (
+   
+    <nav className="navbar">
+      <ul className="nav-items">
+        <li>Home</li>
+        <li>Products</li>
+        <li>Card</li>
+        <div className="search-bar">
+          <input type="text" placeholder='Search' />
+        </div>
+        <li>Login</li>
+      </ul>
+    </nav>  
+
+  )
+}
+
+export default Navbar
